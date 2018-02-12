1f06470b231ac37700f496670876cd6a03402a3d
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-02-12 11:27:37 +0200

    hello world on react

--- calendar/src/app.js
+++ calendar/src/app.js
@@ -1,1 +1,18 @@
-console.log("hello");
+import React from "react";
+import ReactDOM from 'react-dom';
+
+
+class HelloMessage extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <HelloMessage/>,
+    document.getElementById('root')
+);
