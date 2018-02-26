ea3608ea8b00dd8d0a5f6b21d3a8bc2d984c3803
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-02-26 10:03:03 +0200

    Delete calendar directory and add it`s components to project.

--- calendar/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import CalendarLayout from "./components/CalendarLayout";
-import "./styles/common.less"
-
-ReactDOM.render(
-    <CalendarLayout/>,
-    document.getElementById("root")
-);
