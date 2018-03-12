bdea1909700da3945f6387c24e449acec804360c
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-03-12 17:36:31 +0200

    Fullname display.

--- src/components/Person.js
+++ src/components/Person.js
@@ -16,19 +16,18 @@ class Person extends Component {
             "y": this.props.person.y
         }
     }
-
     getMargins(){
         return {
             "marginLeft": this.state.x + 400,
             "marginTop": this.state.y - 500
         }
     }
-
     render() {
+        const fullName = this.state.user.name + " " + this.state.user.surname;
         return (
             <div className={b()} style={this.getMargins()}>
                 <img className={b("img")} src={this.state.user.photo} alt=""/>
-                <h3 className={b("fullName")}>{this.state.user.name} {this.state.user.surname}</h3>
+                <h3 className={b("fullName")} data-text={fullName}>{this.state.user.name} {this.state.user.surname}</h3>
                 <time className={b("birthday")}>{this.state.user.birthday}</time>
                 <time className={b("death")}>{this.state.user.death}</time>
             </div>
