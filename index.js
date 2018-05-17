d4f305ce2bec04292965903ea9203bad2499a59c
RoksolyanaLebedenko <lebedenko@ucu.edu.ua>
2018-05-17 20:24:28 +0300

    Restructure application.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
-const db = require("./db")
+const db = require("./api/db")
 
 const MONOGO_URL = process.env.MONGODB_URI || "mongodb://localhost/family-tree"
 const PORT = process.env.PORT || 3000
@@ -13,15 +13,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 //TODO: create api Router;
 
-app.use("/api", require("./routers/api"))
+app.use("/api", require("./api/routers/api"))
 
+app.use(express.static(path.resolve("build")));
 
-console.log(path.resolve("build"));
-
-app.use("/build", express.static(path.resolve("build")))
-app.use("/*", (req, res) => res.sendFile(path.resolve("index.html")))
-
-
+app.use("/*",  (req, res) => res.sendFile(path.join(__dirname, "build", "index.html")))
 
 db.connect(MONOGO_URL, err => {
   if (err) return console.log("Unable to connect to Mongo.")
